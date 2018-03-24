8b603c1cc562a847f9f7a286378e5d7d45c1b4b4
sinstarzs <32948615+sinstarzs@users.noreply.github.com>
2018-03-24 23:05:30 +0800

    Created map & player

---
+++ New Unity Project/Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject followTarget;
+	private Vector3 targetPos;
+	public float moveSpeed = 5.0f;
+	public float offset = -12.0f;
+
+	// Use this for initialization
+	void Start () {
+		targetPos.z = offset;
+	}
+
+	// Update is called once per frame
+	void LateUpdate () {
+		targetPos.x = followTarget.transform.position.x;
+		targetPos.y = followTarget.transform.position.y;
+		transform.position = Vector3.Lerp (transform.position,targetPos,moveSpeed*Time.deltaTime);
+	}
+}
---
+++ New Unity Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float moveSpeed = 5;
+	public float moveX;
+	public float moveY;
+
+	private Animator anim;
+	private Rigidbody2D rigidBody;
+
+	private bool playerMoving;
+	private Vector2 lastMove;
+
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+		rigidBody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		moveX = Input.GetAxisRaw ("Horizontal");	// moveX = 1, 0 or -1
+		moveY = Input.GetAxisRaw ("Vertical");		// moveY = 1, 0 or -1
+		playerMoving = false;
+
+		//rigidBody.velocity = new Vector2(
+		//	moveX * moveSpeed * (1.0f-0.2f*Mathf.Abs(moveY)),
+		//	moveY * moveSpeed * (1.0f-0.2f*Mathf.Abs(moveX)));
+
+		if (moveX != 0 || moveY != 0) 
+		{
+			transform.Translate (new Vector3(
+				moveX * moveSpeed * Time.deltaTime * (1.0f-0.2f*Mathf.Abs(moveY)),	// (1.0f-0.2f) is to calibrate diagonal move speed
+				moveY * moveSpeed * Time.deltaTime * (1.0f-0.2f*Mathf.Abs(moveX))));
+			playerMoving = true;
+			lastMove = new Vector2 (moveX, moveY);	// only updated when moved
+		}
+
+		anim.SetFloat ("MoveX", moveX);
+		anim.SetFloat ("MoveY", moveY);
+		anim.SetBool ("PlayerMoving", playerMoving);
+		anim.SetFloat ("LastMoveX", lastMove.x);
+		anim.SetFloat ("LastMoveY", lastMove.y);
+	}
+}
